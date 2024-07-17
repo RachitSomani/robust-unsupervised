4acbf8b8aaadceebb3cfac52f4db70467845eecc
RachitSomani <94782888+RachitSomani@users.noreply.github.com>
2024-07-17 07:11:08 +0530

    Update loss_function.py

--- robust_unsupervised/loss_function.py
+++ robust_unsupervised/loss_function.py
@@ -24,7 +24,7 @@ class MultiscaleLPIPS:
 
         return self.lpips_network(x, y, normalize=True).mean() 
 
-    def __call__(self, f_hat, x_clean: Tensor, y: Tensor, mask: Optional[Tensor] = None, consistency_weight: float = 0.1):
+    def __call__(self, f_hat, x_clean: Tensor, y: Tensor, mask: Optional[Tensor] = None, consistency_weight: float = 0.5):
         x = f_hat(x_clean)
 
         losses = []
@@ -32,11 +32,11 @@ class MultiscaleLPIPS:
         if mask is not None:
             mask = F.interpolate(mask, y.shape[-1], mode="area")
 
-        # x_perturbed = x_clean + torch.randn_like(x_clean) * 0.01  # Add small perturbations
-        # x_perturbed = f_hat(x_perturbed)
-        # consistency_loss = F.l1_loss(x, x_perturbed)
+        x_perturbed = x_clean + torch.randn_like(x_clean) * 0.01  # Add small perturbations
+        x_perturbed = f_hat(x_perturbed)
+        consistency_loss = F.l1_loss(x, x_perturbed)
 
-        # x_perturbed = F.interpolate(x_perturbed, size=y.shape[-2:], mode='bilinear', align_corners=False)
+        x_perturbed = F.interpolate(x_perturbed, size=y.shape[-2:], mode='bilinear', align_corners=False)
             
         
         for weight in self.weights:
@@ -46,9 +46,9 @@ class MultiscaleLPIPS:
             
             if weight > 0:
                 loss_x = self.measure_lpips(x, y, mask)
-                # loss_x_perturbed = self.measure_lpips(x_perturbed, y, mask)
-                # symmetric_loss = (loss_x + loss_x_perturbed) / 2.0
-                losses.append(weight * loss_x) #symmetric_loss)
+                loss_x_perturbed = self.measure_lpips(x_perturbed, y, mask)
+                symmetric_loss = (loss_x + loss_x_perturbed) / 2.0
+                losses.append(weight * symmetric_loss)
 
             if mask is not None:
                 mask = F.avg_pool2d(mask, 2)
@@ -56,10 +56,10 @@ class MultiscaleLPIPS:
             x = F.avg_pool2d(x, 2)
             x_clean = F.avg_pool2d(x_clean, 2)
             y = F.avg_pool2d(y, 2)
-            # x_perturbed=F.avg_pool2d(x_perturbed,2)
+            x_perturbed=F.avg_pool2d(x_perturbed,2)
         
         total = torch.stack(losses).sum(dim=0) if len(losses) > 0 else 0.0
         l1 = self.l1_weight * F.l1_loss(x, y)
-        total_loss = total + l1 #+ consistency_loss
+        total_loss = total + l1 + consistency_weight * consistency_loss
         return total_loss
         
