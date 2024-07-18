be4d83d0f95a1269d8cf07b14f0474215dae1a9e
RachitSomani <94782888+RachitSomani@users.noreply.github.com>
2024-07-18 17:02:54 +0530

    Update loss_function.py

--- robust_unsupervised/loss_function.py
+++ robust_unsupervised/loss_function.py
@@ -27,7 +27,12 @@ class MultiscaleLPIPS:
         return self.lpips_network(x, y, normalize=True).mean() 
 
     def ssim_loss(self, pred, target):
-        return 1 - ssim(pred, target, data_range=1.0, size_average=True)
+        min_size = min(pred.size(-2), pred.size(-1))
+        win_size = min(11, min_size)  # Use 11 or smaller if the image is smaller
+        # Ensure win_size is odd
+        win_size = win_size - 1 if win_size % 2 == 0 else win_size
+        
+        return 1 - ssim(pred, target, data_range=1.0, size_average=True, win_size=win_size)
 
     def __call__(self, f_hat, x_clean: Tensor, y: Tensor, mask: Optional[Tensor] = None, consistency_weight: float = 0.3):
         x = f_hat(x_clean)
