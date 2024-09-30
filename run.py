9f76d657bff049683b4dffc0f229a262986c1f68
RachitSomani <94782888+RachitSomani@users.noreply.github.com>
2024-09-30 15:45:42 +0530

    Update run.py

--- run.py
+++ run.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
         raise Exception("Invalid task name")
     
     tasks = [
-        benchmark.get_task("inpainting", "XL")
+        benchmark.get_task("inpainting", "L")
     ]
 
     for task in tasks:
