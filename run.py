1a417e3af6cff7833091d72bcc6b5d5466e69b98
RachitSomani <94782888+RachitSomani@users.noreply.github.com>
2024-10-11 23:36:15 +0530

    Update run.py

--- run.py
+++ run.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
         raise Exception("Invalid task name")
     
     tasks = [
-        benchmark.get_task("upsampling", "S")
+        benchmark.get_task("upsampling", "XS")
     ]
 
     for task in tasks:
