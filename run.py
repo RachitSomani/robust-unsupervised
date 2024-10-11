ccfd7c379fc4057bf442b5bd220b51bfd5eff63f
RachitSomani <94782888+RachitSomani@users.noreply.github.com>
2024-10-11 21:08:27 +0530

    Update run.py

--- run.py
+++ run.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
         raise Exception("Invalid task name")
     
     tasks = [
-        benchmark.get_task("denoising", "S")
+        benchmark.get_task("denoising", "XS")
     ]
 
     for task in tasks:
