73e2fd99af27cde780b83d10689216eb809e2a64
RachitSomani <94782888+RachitSomani@users.noreply.github.com>
2024-10-15 11:17:29 +0530

    Update run.py

--- run.py
+++ run.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
         raise Exception("Invalid task name")
     
     tasks = [
-        benchmark.get_task("deartifacting", "M")
+        benchmark.get_task("deartifacting", "S")
     ]
 
     for task in tasks:
